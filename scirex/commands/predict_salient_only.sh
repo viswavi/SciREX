e413e8c2dc460983985674667d9135e5814c2c48
Vijay Viswanathan <vcomeinpeace@gmail.com>
2020-12-10 15:48:12 -0500

    Add scripts for salient-only eval

---
+++ scirex/commands/predict_salient_only.sh
@@ -0,0 +1,34 @@
+export test_file=scirex_dataset/release_data/test.jsonl
+export test_output_folder=test_outputs/
+
+
+# These scripts require you've already generated files for NER and cluster predictions
+if [ ! -f $test_output_folder/cluster_predictions.jsonl ]; then
+    echo "Need to generate $test_output_folder/cluster_predictions.jsonl, from the general SciREX prediction script"
+    exit 1
+elif [ ! -f $test_output_folder/ner_predictions.jsonl ]; then
+    echo "Need to generate $test_output_folder/ner_predictions.jsonl, from the general SciREX prediction script"
+    exit 1
+fi
+
+
+echo "Predicting Salient Mentions"
+python3.7 scirex/predictors/predict_salient_mentions.py \
+$salient_only_archive \
+$test_output_folder/ner_predictions.jsonl \
+$test_output_folder/salient_mentions_predictions.jsonl \
+$cuda_device
+
+echo "Predicting Salient Clustering "
+python3.7  scirex/predictors/predict_salient_clusters.py \
+$test_output_folder/cluster_predictions.jsonl \
+$test_output_folder/salient_mentions_predictions.jsonl \
+$test_output_folder/salient_clusters_predictions.jsonl
+
+
+echo "Evaluating on all Predicted steps "
+python3.7  scirex/evaluation_scripts/salient_only_evaluate.py \
+--gold-file $test_file \
+--ner-file $test_output_folder/ner_predictions.jsonl \
+--salient-mentions-file $test_output_folder/salient_mentions_predictions.jsonl \
+--clusters-file $test_output_folder/salient_clusters_predictions.jsonl \
