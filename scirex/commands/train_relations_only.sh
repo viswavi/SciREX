040f9cb8dc133fa80d3f60ff6d05a3b819089f72
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-01-13 15:32:29 -0500

    Add training script for relations-only model

--- scirex/commands/train_relations_only.sh
+++ scirex/commands/train_relations_only.sh
@@ -7,7 +7,7 @@ fi
 export BERT_VOCAB=$BERT_BASE_FOLDER/vocab.txt
 export BERT_WEIGHTS=$BERT_BASE_FOLDER/weights.tar.gz
 
-export CONFIG_FILE=scirex/training_config/ner_only_main.jsonnet
+export CONFIG_FILE=scirex/training_config/relations_only_main.jsonnet
 
 export CUDA_DEVICE=$CUDA_DEVICE
 
@@ -19,15 +19,10 @@ export TRAIN_PATH=$DATA_BASE_PATH/train.jsonl
 export DEV_PATH=$DATA_BASE_PATH/dev.jsonl
 export TEST_PATH=$DATA_BASE_PATH/test.jsonl
 
-export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_ner_only_with_graph_embeddings/$1}
+export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_relations_only/$1}
 
 export bert_fine_tune=10,11,pooler
 
-export use_citation_graph_embeddings=true
-export citation_embedding_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/embeddings.npy
-export doc_to_idx_mapping_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/scirex_docids.json
 nw=1 lw=1 rw=1 em=false \
 relation_cardinality=4 \
-citation_embedding_file="/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/embeddings.npy" \
-doc_to_idx_mapping_file="/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/scirex_docids.json" \
 allennlp train -s $OUTPUT_BASE_PATH --include-package scirex $RECOVER $CONFIG_FILE
