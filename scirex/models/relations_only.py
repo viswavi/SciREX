4b4da53ce9c8fc04b4ce4551f1588cbfd25f3c78
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-01-13 18:59:08 -0500

    Create RelationsOnly model

--- scirex/models/relations_only.py
+++ scirex/models/relations_only.py
@@ -1,7 +1,5 @@
 import copy
-import json
 import logging
-import numpy as np
 from typing import Dict, List, Optional
 
 import torch
@@ -17,19 +15,15 @@ from overrides import overrides
 
 # Import submodules.
 from scirex.models.relations.entity_relation import RelationExtractor as NAryRelationExtractor
-from scirex.models.ner.ner_crf_tagger import NERTagger
 from scirex.models.span_classifiers.span_classifier import SpanClassifier
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
-@Model.register("ner_only")
-class NEROnlyModel(Model):
+@Model.register("relations_only")
+class RelationsOnlyModel(Model):
     def __init__(
         self,
-        use_citation_graph_embeddings: str,
-        citation_embedding_file: str,
-        doc_to_idx_mapping_file: str,
         vocab: Vocabulary,
         text_field_embedder: TextFieldEmbedder,
         context_layer: Seq2SeqEncoder,
@@ -40,27 +34,22 @@ class NEROnlyModel(Model):
         regularizer: Optional[RegularizerApplicator] = None,
         display_metrics: List[str] = None,
     ) -> None:
-        super(NEROnlyModel, self).__init__(vocab, regularizer)
+        super(RelationsOnlyModel, self).__init__(vocab, regularizer)
 
         self._text_field_embedder = text_field_embedder
         self._context_layer = context_layer
         self._lexical_dropout = torch.nn.Dropout(p=lexical_dropout)
 
-        if use_citation_graph_embeddings:
-            if citation_embedding_file == "" or doc_to_idx_mapping_file == "":
-                raise ValueError("Must supply citation embedding files to use graph embedding features")
-            self._document_embedding = initialize_graph_embeddings(citation_embedding_file, finetune_embedding=False)
-            self._doc_to_idx_mapping = json.load(open(doc_to_idx_mapping_file))
-        else:
-            self._document_embedding = None
-            self._doc_to_idx_mapping = None
-
         modules = Params(modules)
 
-        self._ner = NERTagger.from_params(vocab=vocab,
-                                          document_embedding=self._document_embedding,
-                                          doc_to_idx_mapping=self._doc_to_idx_mapping,
-                                          params=modules.pop("ner"))
+        self._cluster_n_ary_relation = NAryRelationExtractor.from_params(
+            vocab=vocab, params=modules.pop("n_ary_relation")
+        )
+
+        self._endpoint_span_extractor = EndpointSpanExtractor(
+            context_layer.get_output_dim(), combination="x,y"
+        )
+        self._attentive_span_extractor = SelfAttentiveSpanExtractor(input_dim=context_layer.get_output_dim())
 
         for k in loss_weights:
             loss_weights[k] = float(loss_weights[k])
@@ -68,7 +57,7 @@ class NEROnlyModel(Model):
         self._permanent_loss_weights = copy.deepcopy(self._loss_weights)
 
         self._display_metrics = display_metrics
-        self._multi_task_loss_metrics = {k: Average() for k in ["ner"]}
+        self._multi_task_loss_metrics = {k: Average() for k in ["n_ary_relation"]}
 
         self.training_mode = True
         self.prediction_mode = False
@@ -92,10 +81,17 @@ class NEROnlyModel(Model):
         loss = 0.0
 
         output_embedding = self.embedding_forward(text)
-
-        if self._loss_weights["ner"] > 0.0:
-            output_dict["ner"] = self.ner_forward(output_embedding=output_embedding, ner_type_labels=ner_type_labels, metadata=metadata)
-            loss += self._loss_weights["ner"] * output_dict["ner"]["loss"]
+        output_span_embedding = self.span_embeddings_forward(
+            output_embedding, spans, span_type_labels, span_features, metadata
+        )
+        if self._loss_weights["n_ary_relation"] > 0.0:
+            output_dict["n_ary_relation"] = self.relation_forward(
+                output_span_embedding,
+                metadata,
+                relation_to_cluster_ids,
+                span_cluster_labels,
+            )
+            loss += self._loss_weights["n_ary_relation"] * output_dict["n_ary_relation"]["loss"]
 
         output_dict["loss"] = loss
         for k in self._multi_task_loss_metrics:
@@ -141,25 +137,128 @@ class NEROnlyModel(Model):
             "lengths": sentence_lengths,
         }
         return output_embedding
+        
 
-    def ner_forward(self, output_embedding, ner_type_labels, metadata):
-        output_ner = {"loss": 0.0}
+    def span_embeddings_forward(self, output_embedding, spans, span_type_labels, span_features, metadata):
+        output_span_embeddings = {"valid": False}
 
-        output_ner = self._ner(
-            output_embedding["contextualised"], output_embedding["mask"], ner_type_labels, metadata
+        if spans.nelement() != 0:
+            span_mask, spans, span_embeddings = self.extract_span_embeddings(
+                output_embedding["contextualised"], spans
+            )
+
+            if span_mask.sum() != 0:
+                span_offset = self.offset_span_by_para_start(metadata, spans, span_mask)
+                span_position = self.get_span_position(metadata, span_offset)
+                span_type_labels_one_hot = self.get_span_one_hot_labels(
+                    "span_type_labels", span_type_labels, spans
+                )
+
+                span_features = torch.cat(
+                    [span_position, span_type_labels_one_hot, span_features.float()], dim=-1
+                )
+                featured_span_embeddings = torch.cat([span_embeddings, span_features], dim=-1)
+                span_ix = span_mask.view(-1).nonzero().squeeze(1).long()
+
+                output_span_embeddings = {
+                    "span_mask": span_mask,
+                    "span_ix": span_ix,
+                    "spans": span_offset,
+                    "span_embeddings": span_embeddings,
+                    "featured_span_embeddings": featured_span_embeddings,
+                    "span_type_labels": span_type_labels_one_hot,
+                    "span_features": span_features.float(),
+                    "valid": True,
+                }
+
+        return output_span_embeddings
+
+    def relation_forward(
+        self, output_span_embedding, metadata, relation_to_cluster_ids, span_cluster_labels):
+        output_n_ary_relation = {"loss": 0.0}
+
+        if output_span_embedding["valid"]:
+            spans, featured_span_embeddings, span_ix, span_mask = (
+                output_span_embedding["spans"],
+                output_span_embedding["featured_span_embeddings"],
+                output_span_embedding["span_ix"],
+                output_span_embedding["span_mask"],
+            )
+
+            if relation_to_cluster_ids is not None or self.prediction_mode:
+                n_salient_clusters = len(metadata[0]["document_metadata"]["cluster_name_to_id"])
+                type_to_cluster_ids = metadata[0]["document_metadata"]["type_to_cluster_ids"]
+                relation_to_cluster_ids = metadata[0]["document_metadata"]["relation_to_cluster_ids"]
+                span_cluster_labels = span_cluster_labels[:, :, :n_salient_clusters]
+
+                output_n_ary_relation = self._cluster_n_ary_relation.compute_representations(
+                    span_embeddings=featured_span_embeddings,
+                    coref_labels=span_cluster_labels,
+                    type_to_cluster_ids=type_to_cluster_ids,
+                    relation_to_cluster_ids=relation_to_cluster_ids,
+                    metadata=metadata,
+                )
+
+        return output_n_ary_relation
+
+    def get_span_one_hot_labels(self, label_namespace, span_labels, spans):
+        n_labels = self.vocab.get_vocab_size(label_namespace)
+        span_labels_one_hot = torch.zeros((span_labels.size(0), span_labels.size(1), n_labels)).to(
+            spans.device
         )
+        span_labels_one_hot.scatter_(-1, span_labels.unsqueeze(-1), 1)
+        return span_labels_one_hot
 
-        if self.prediction_mode:
-            output_ner = self._ner.decode(output_ner)
-            output_ner["spans"] = output_ner["spans"].to(output_embedding["text"].device).long()
-            output_ner["span_labels"] = output_ner["span_labels"].to(output_embedding["text"].device).long()
+    @staticmethod
+    def get_span_position(metadata, span_offset):
+        doc_length = metadata[0]["document_metadata"]["doc_length"]
+        span_position = span_offset.float().mean(-1, keepdim=True) / doc_length
+        return span_position
 
-        return output_ner
+    @staticmethod
+    def offset_span_by_para_start(metadata, spans, span_mask):
+        start_pos_in_doc = torch.LongTensor([x["start_pos_in_doc"] for x in metadata]).to(
+            spans.device
+        )  # (B,)
+        para_offset = start_pos_in_doc.unsqueeze(1).unsqueeze(2)  # (B, 1, 1)
+        span_offset = spans + (para_offset * span_mask.unsqueeze(-1).long())
+        return span_offset
+
+    def extract_span_embeddings(self, contextualized_embeddings, spans):
+        attended_span_embeddings = self._attentive_span_extractor(contextualized_embeddings, spans)
+        span_mask = (spans[:, :, 0] >= 0).long()
+        spans = F.relu(spans.float()).long()
+        endpoint_span_embeddings = self._endpoint_span_extractor(contextualized_embeddings, spans)
+        span_embeddings = torch.cat([endpoint_span_embeddings, attended_span_embeddings], -1)
+        return span_mask, spans, span_embeddings
 
     @overrides
     def decode(self, output_dict: Dict[str, torch.Tensor]):
         res = {}
-        res["ner"] = self._ner.decode(output_dict["ner"])
+        res["n_ary_relation"] = self._n_ary_relation.decode(output_dict["n_ary_relation"])
+
+        return res
+
+    def decode_relations(self, batch):
+        output_embedding = self.embedding_forward(text=batch["text"])
+        output_span_embedding = self.span_embeddings_forward(
+            output_embedding=output_embedding,
+            spans=batch["spans"],
+            span_type_labels=batch["span_type_labels"],
+            span_features=batch["span_features"],
+            metadata=batch["metadata"],
+        )
+
+        output_n_ary_relation = self.relation_forward(
+            output_span_embedding=output_span_embedding,
+            metadata=batch["metadata"],
+            relation_to_cluster_ids=batch.get("relation_to_cluster_ids", None),
+            span_cluster_labels=batch["span_cluster_labels"],
+        )
+
+        res = {}
+        res["n_ary_relation"] = self._cluster_n_ary_relation.decode(output_n_ary_relation)
+
         return res
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
@@ -167,23 +266,24 @@ class NEROnlyModel(Model):
         Get all metrics from all modules. For the ones that shouldn't be displayed, prefix their
         keys with an underscore.
         """
-        metrics_ner = self._ner.get_metrics(reset=reset)
+        metrics_n_ary = self._cluster_n_ary_relation.get_metrics(reset=reset)
         metrics_loss = {"loss_" + k: v.get_metric(reset) for k, v in self._multi_task_loss_metrics.items()}
         metrics_loss = {k: (v.item() if hasattr(v, "item") else v) for k, v in metrics_loss.items()}
 
         # Make sure that there aren't any conflicting names.
         metric_names = (
-            list(metrics_ner.keys())
+            list(metrics_n_ary.keys())
             + list(metrics_loss.keys())
         )
         assert len(set(metric_names)) == len(metric_names)
         all_metrics = dict(
-            list(metrics_ner.items())
+            list(metrics_n_ary.items())
             + list(metrics_loss.items())
         )
 
         all_metrics["validation_metric"] = (
-            self._loss_weights["ner"] * nan_to_zero(metrics_ner.get("ner_f1-measure", 0))
+            self._loss_weights["n_ary_relation"]
+            * nan_to_zero(metrics_n_ary.get("n_ary_rel_global_1.f1-score", 0))
         )
 
         self._display_metrics.append("validation_metric")
@@ -206,14 +306,3 @@ def nan_to_zero(n):
         return 0
 
     return n
-
-def initialize_graph_embeddings(graph_embedding_file, finetune_embedding=False):
-    """
-    Initialize graph embedding lookup table, loaded from file.
-    """
-    graph_embedding_array = np.load(graph_embedding_file)
-    num_embeddings, embedding_dim = graph_embedding_array.shape
-    embedding = torch.nn.Embedding(num_embeddings = num_embeddings, embedding_dim = embedding_dim)
-    embedding.weight.data.copy_(torch.from_numpy(graph_embedding_array))
-    embedding.weight.requires_grad = finetune_embedding
-    return embedding 
