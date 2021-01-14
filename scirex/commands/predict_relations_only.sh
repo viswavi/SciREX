e24e4fd13a45424e415396eb31f0a4ba4abd4d02
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-01-14 08:56:35 -0500

    Add graph_embeddings suffix to predict_relations script

--- scirex/commands/predict_relations_only.sh
+++ scirex/commands/predict_relations_only.sh
@@ -22,7 +22,7 @@ python scirex/predictors/predict_n_ary_relations.py \
 $relations_only_archive \
 $test_file \
 $test_file \
-$test_output_folder/relations_predictions_gold_clusters.jsonl \
+$test_output_folder/relations_predictions_gold_clusters_with_graph_embeddings.jsonl \
 $cuda_device
 
 echo "Evaluating on all Predicted steps "
@@ -30,4 +30,4 @@ python scirex/evaluation_scripts/relations_only_evaluate.py \
 --gold-file $test_file \
 --ner-file $test_file \
 --clusters-file $test_file \
---relations-file $test_output_folder/relations_predictions_gold_clusters.jsonl
+--relations-file $test_output_folder/relations_predictions_gold_clusters_with_graph_embeddings.jsonl
