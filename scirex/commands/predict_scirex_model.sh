29c4f566b3340961ff1e5602896fbda84606672f
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-01-17 18:35:33 -0500

    Revise model and output paths to be unique

--- scirex/commands/predict_scirex_model.sh
+++ scirex/commands/predict_scirex_model.sh
@@ -1,5 +1,5 @@
 export test_file=scirex_dataset/release_data/test.jsonl
-export test_output_folder=test_outputs/
+export test_output_folder=test_outputs_from_our_paper/
 
 echo "Predicting NER"
 python scirex/predictors/predict_ner.py \
--- scirex/commands/train_scirex_model.sh
+++ scirex/commands/train_scirex_model.sh
@@ -19,7 +19,7 @@ export TRAIN_PATH=$DATA_BASE_PATH/train.jsonl
 export DEV_PATH=$DATA_BASE_PATH/dev.jsonl
 export TEST_PATH=$DATA_BASE_PATH/test.jsonl
 
-export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_scirex_full/$1}
+export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_scirex_full_graph/$1}
 
 export bert_fine_tune=10,11,pooler
 export finetune_embedding=false
