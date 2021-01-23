586efca8949d84aa9410ff2462ddb61fa91a2922
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-01-22 23:23:21 -0500

    Save

--- scirex/commands/train_relations_only.sh
+++ scirex/commands/train_relations_only.sh
@@ -19,13 +19,14 @@ export TRAIN_PATH=$DATA_BASE_PATH/train.jsonl
 export DEV_PATH=$DATA_BASE_PATH/dev.jsonl
 export TEST_PATH=$DATA_BASE_PATH/test.jsonl
 
-export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_relations_only_with_graph_embeddings_early_fusion_and_citances/$1}
+default_output_dir=outputs/pwc_outputs/experiment_relations_only_with_graph_early_and_citances_${pytorch_seed}/$1
+export OUTPUT_BASE_PATH=${OUTPUT_DIR:-$default_output_dir}
 
 export bert_fine_tune=10,11,pooler
 
 export use_citation_graph_embeddings=true
-export citation_embedding_file=/projects/ogma1/vijayv/SciREX/graph_embeddings/embeddings.npy
-export doc_to_idx_mapping_file=/projects/ogma1/vijayv/SciREX/graph_embeddings/scirex_docids.json
+export citation_embedding_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/embeddings.npy
+export doc_to_idx_mapping_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/scirex_docids.json
 nw=1 lw=1 rw=1 em=false \
 relation_cardinality=4 \
 allennlp train -s $OUTPUT_BASE_PATH --include-package scirex $RECOVER $CONFIG_FILE
