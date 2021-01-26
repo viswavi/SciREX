263574d0182e844891119ea561c5e8117e2335d6
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2021-01-26 07:41:44 -0500

    Skip ner output for citance chunks uniformly

--- scirex/models/scirex_model.py
+++ scirex/models/scirex_model.py
@@ -90,10 +90,16 @@ class ScirexModel(Model):
 
         if self._loss_weights["ner"] > 0.0:
             ner_output = self.ner_forward(output_embedding=output_embedding, ner_type_labels=ner_type_labels, metadata=metadata)
-            loss += self._loss_weights["ner"] * output_dict["ner"]["loss"]
+
             body_text = [len([token for token in x["paragraph"] if "CITE" in token]) == 0 for x in ner_output["metadata"]]
             if True not in body_text:
                 # All citances; just skip this section entirely
+                for i in range(len(body_text)):
+                    original_masks = output_embedding["mask"]
+                    sum_mask = sum(original_masks[i])
+                    ner_output["gold_tags"].insert(i, [0] * sum_mask)
+                    ner_output["tags"].insert(i, [0] * sum_mask)
+                output_dict["ner"] = ner_output
                 loss += torch.tensor(0.0, device=device, requires_grad=True)
             else: # Not all citances.
                 if False in body_text:
