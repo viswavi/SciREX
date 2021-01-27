874eed067dc5d65dc8cb3522a65acfd7b4087be1
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-01-26 23:36:42 -0500

    Add function to perform hierarchical paired bootstrap

--- scirex/metrics/paired_bootstrap.py
+++ scirex/metrics/paired_bootstrap.py
@@ -139,3 +139,84 @@ def eval_with_paired_bootstrap(gold, sys1, sys2,
     p_value_lose = 1-wins[0]
     p_value_win = 1-wins[1]
     return sys1_summary, sys2_summary, p_value_lose, p_value_win
+
+def eval_with_hierarchical_paired_bootstrap(gold, sys1_list, sys2_list,
+                               num_samples=10000, sample_ratio=0.5,
+                               eval_type='acc',
+                               return_results=False):
+  ''' Evaluate with a hierarchical paired boostrap
+  This compares two systems, performing a significance tests with
+  paired bootstrap resampling to compare the accuracy of the two systems, with
+  two-level sampling: first we sample a model, then we sample data to evaluate
+  it on.
+
+  :param gold: The correct labels
+  :param sys1: The output of system 1
+  :param sys2: The output of system 2
+  :param num_samples: The number of bootstrap samples to take
+  :param sample_ratio: The ratio of samples to take every time
+  :param eval_type: The type of evaluation to do (acc, pearson, bleu, bleu_detok)
+  '''
+  for sys1 in sys1_list:
+    assert(len(gold) == len(sys1))
+  for sys2 in sys2_list:
+    assert(len(gold) == len(sys2))
+
+  # Preprocess the data appropriately for they type of eval
+  gold = [eval_preproc(x, eval_type) for x in gold]
+  sys1_list = [[eval_preproc(x, eval_type) for x in sys1] for sys1 in sys1_list]
+  sys2_list = [[eval_preproc(x, eval_type) for x in sys2] for sys2 in sys2_list]
+
+  sys1_scores = []
+  sys2_scores = []
+  wins = [0, 0, 0]
+  n = len(gold)
+  ids = list(range(n))
+
+  for _ in tqdm(range(num_samples)):
+    # Subsample the gold and system outputs
+    np.random.shuffle(ids)
+    reduced_ids = ids[:int(len(ids)*sample_ratio)]
+
+    sys1_idx = np.random.choice(list(range(len(sys1_list))))
+    sys1 = sys1_list[sys1_idx]
+
+    sys2_idx = np.random.choice(list(range(len(sys2_list))))
+    sys2 = sys2_list[sys2_idx]
+
+    reduced_gold = [gold[i] for i in reduced_ids]
+    reduced_sys1 = [sys1[i] for i in reduced_ids]
+    reduced_sys2 = [sys2[i] for i in reduced_ids]
+    # Calculate accuracy on the reduced sample and save stats
+    sys1_score = eval_measure(reduced_gold, reduced_sys1, eval_type=eval_type)
+    sys2_score = eval_measure(reduced_gold, reduced_sys2, eval_type=eval_type)
+    if sys1_score > sys2_score:
+      wins[0] += 1
+    elif sys1_score < sys2_score:
+      wins[1] += 1
+    else:
+      wins[2] += 1
+    sys1_scores.append(sys1_score)
+    sys2_scores.append(sys2_score)
+
+  # Print win stats
+  wins = [x/float(num_samples) for x in wins]
+  print('Win ratio: sys1=%.3f, sys2=%.3f, tie=%.3f' % (wins[0], wins[1], wins[2]))
+  if wins[0] > wins[1]:
+    print('(sys1 is superior with p value p=%.10f)\n' % (1-wins[0]))
+  elif wins[1] > wins[0]:
+    print('(sys2 is superior with p value p=%.10f)\n' % (1-wins[1]))
+
+  # Print system stats
+  sys1_scores.sort()
+  sys2_scores.sort()
+  print('sys1 mean=%.3f, median=%.3f, 95%% confidence interval=[%.3f, %.3f]' %
+          (np.mean(sys1_scores), np.median(sys1_scores), sys1_scores[int(num_samples * 0.025)], sys1_scores[int(num_samples * 0.975)]))
+  print('sys2 mean=%.3f, median=%.3f, 95%% confidence interval=[%.3f, %.3f]' %
+          (np.mean(sys2_scores), np.median(sys2_scores), sys2_scores[int(num_samples * 0.025)], sys2_scores[int(num_samples * 0.975)]))
+  if return_results:
+    sys1_summary = (np.mean(sys1_scores), (sys1_scores[int(num_samples * 0.025)], sys1_scores[int(num_samples * 0.975)]))
+    sys2_summary = (np.mean(sys2_scores), (sys2_scores[int(num_samples * 0.025)], sys2_scores[int(num_samples * 0.975)]))
+    p_value_lose = 1-wins[0]
+    p_value_win = 1-wins[1]
+    return sys1_summary, sys2_summary, p_value_lose, p_value_win
