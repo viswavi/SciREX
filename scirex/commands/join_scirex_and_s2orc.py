fb1a92e7777b6444ad0c8ab5953b120a231745ce
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-01-31 02:45:49 -0500

    Add analysis and visualization functions for citation graph

--- scirex/commands/join_scirex_and_s2orc.py
+++ scirex/commands/join_scirex_and_s2orc.py
@@ -3,6 +3,7 @@ import gzip
 import json
 import jsonlines
 import os
+import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import random
@@ -545,20 +546,53 @@ def get_citation_graph(radius, remap_to_scirex_id = False):
     return out_edges_scirex_keys, in_edges_scirex_keys
 
 
-def compute_scirex_documents_graph_degrees(remap_to_scirex_id = True):
+def compute_scirex_documents_graph_degrees(remap_to_scirex_id = True, degree_direction="both"):
+    assert degree_direction in ["both", "out", "in"]
+
     in_graph, out_graph = get_citation_graph(1, remap_to_scirex_id=remap_to_scirex_id)
     scirex_paper_ids = list(get_scirex_docids())
     degrees = {}
     for doc_id in scirex_paper_ids:
         degrees[doc_id] = 0
-        degrees[doc_id] += len(in_graph.get(doc_id, []))
-        degrees[doc_id] += len(out_graph.get(doc_id, []))
+        if degree_direction == "both" or degree_direction == "in":
+            degrees[doc_id] += len(in_graph.get(doc_id, []))
+        if degree_direction == "both" or degree_direction == "out":
+            degrees[doc_id] += len(out_graph.get(doc_id, []))
     return degrees
 
 
+def draw_bar_chart(data, ylabel, xlabel=None, fname="/tmp/scratch.png", num_buckets=100, xlimit=None):
+    fig, ax = plt.subplots()
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    if xlimit:
+        ax.set_xlim(0, xlimit)
+    n, bins, patches = ax.hist(data, num_buckets, color="lightblue", rwidth=0.8)
+    print(f"Wrote figure to {fname}")
+    fig.savefig(fname, dpi=400, bbox_inches='tight')
+    plt.tight_layout()
+    del fig
 
-def bucket_documents_by_graph_degree(test_set, num_buckets=6, remap_to_scirex_id = True):
-    all_degrees = compute_scirex_documents_graph_degrees(remap_to_scirex_id=remap_to_scirex_id)
+def plot_histogram_of_degrees():
+    out_graph, in_graph = get_citation_graph(1, remap_to_scirex_id=True)
+    scirex_paper_ids = list(get_scirex_docids())
+    in_degrees = {}
+    out_degrees={}
+    for doc_id in scirex_paper_ids:
+        in_degrees[doc_id] = 0
+        out_degrees[doc_id] = 0
+        in_degrees[doc_id] += len(in_graph.get(doc_id, []))
+        out_degrees[doc_id] += len(out_graph.get(doc_id, []))
+
+    in_degree_counts = list(in_degrees.values())
+    out_degree_counts = list(out_degrees.values())
+
+    draw_bar_chart(out_degree_counts, "Frequency",  fname="/tmp/out_degree_histogram.pdf", num_buckets=30)
+    draw_bar_chart(in_degree_counts, "Frequency", fname="/tmp/in_degree_histogram.pdf", xlimit=2500, num_buckets=350)
+
+def bucket_documents_by_graph_degree(test_set, num_buckets=6, degree_direction="both", remap_to_scirex_id = True):
+    all_degrees = compute_scirex_documents_graph_degrees(remap_to_scirex_id=remap_to_scirex_id, degree_direction=degree_direction)
     test_degrees = {}
     for doc in test_set:
         test_degrees[doc] = all_degrees.get(doc, 0)
@@ -575,6 +609,21 @@ def bucket_documents_by_graph_degree(test_set, num_buckets=6, remap_to_scirex_id
         if degree > end:
             end = degree
         buckets[-1] = ((start, end), docs)
+    return buckets
+
+
+def bucket_documents_by_manual_buckets(test_set, buckets, degree_direction="both", remap_to_scirex_id = True):
+    all_degrees = compute_scirex_documents_graph_degrees(remap_to_scirex_id=remap_to_scirex_id, degree_direction=degree_direction)
+    buckets = [(bucket, []) for bucket in buckets]
+    for doc in test_set:
+        graph_degree = all_degrees.get(doc, 0)
+        for i in range(len(buckets)):
+            bucket_start = buckets[i][0][0]
+            bucket_end = buckets[i][0][1]
+            if graph_degree >= bucket_start and (graph_degree < bucket_end or (graph_degree <= bucket_end and i == len(buckets) - 1)):
+                buckets[i][1].append(doc)
+                break
+    assert set([x for y in buckets for x in y[1]]) == set(test_set), breakpoint()
     return buckets
 
 
