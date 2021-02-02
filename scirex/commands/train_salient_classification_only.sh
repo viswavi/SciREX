5fb6bd6dc4cd9f8ff4ce74847a7a2dff1d3885d7
Vijay Viswanathan <vcomeinpeace@gmail.com>
2021-02-02 03:48:30 -0500

    Rename evaluation and visualization scripts

--- scirex/commands/train_salient_classification_only.sh
+++ scirex/commands/train_salient_classification_only.sh
@@ -23,12 +23,11 @@ export OUTPUT_BASE_PATH=${OUTPUT_DIR:-outputs/pwc_outputs/experiment_salient_onl
 
 export bert_fine_tune=10,11,pooler
 export finetune_embedding=false
-export citation_embedding_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/embeddings.npy
-export doc_to_idx_mapping_file=/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/scirex_docids.json
-
+if [ -z citation_embedding_file ]; then
+    citation_embedding_file=""
+    doc_to_idx_mapping_file="";
+fi
 nw=1 lw=1 rw=1 em=false \
 relation_cardinality=4 \
 use_citation_graph_embeddings=true \
-citation_embedding_file="/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/embeddings.npy" \
-doc_to_idx_mapping_file="/projects/metis0_ssd/users/vijayv/SciREX/graph_embeddings/scirex_docids.json" \
 allennlp train -s $OUTPUT_BASE_PATH --include-package scirex $RECOVER $CONFIG_FILE
